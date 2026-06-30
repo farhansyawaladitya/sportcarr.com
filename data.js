ee109bca607a33516bedb8efae19eea9bd9978f5
farhansyawaladitya <farhangt580@gmail.com>
2026-06-30 11:49:42 +0700

    data

---
+++ data.js
@@ -0,0 +1,61 @@
+const cars=[
+
+{
+nama:"Mercedes-AMG GT",
+harga:3,
+kecepatan:10,
+hp:10,
+efisiensi:9,
+desain:7,
+skor:44,
+ranking:1,
+gambar:"img/mercedes-amg-gt.jpg"
+},
+
+{
+nama:"Audi R8",
+harga:6,
+kecepatan:10,
+hp:9,
+efisiensi:8,
+desain:8,
+skor:40,
+ranking:2,
+gambar:"img/audi-r8.jpg"
+},
+
+{
+nama:"Toyota Supra",
+harga:5,
+kecepatan:9,
+hp:6,
+efisiensi:9,
+desain:9,
+skor:39,
+ranking:3,
+gambar:"img/toyota-supra.jpg"
+},
+
+{
+nama:"Jaguar F-Type",
+harga:4,
+kecepatan:10,
+hp:10,
+efisiensi:4,
+desain:8,
+skor:39,
+ranking:4
+},
+
+{
+nama:"BMW M4",
+harga:5,
+kecepatan:9,
+hp:8,
+efisiensi:7,
+desain:8,
+skor:37,
+ranking:5
+}
+
+];
